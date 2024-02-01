f36f29495e04f271650179861e2c6b7c4ff030d4
Arthur <arthrp@users.noreply.github.com>
2024-02-02 00:46:46 +0200

    Add more games

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ export default function About() {
         <div className="container">
             <h1  className="text-center mt-5">About</h1>
             
-            <p id="aboutDesc">The goal is to provide a list of open-source games (both reimplementations that require original game data and new titles).</p>
+            <p id="aboutDesc">The goal is to provide a list of open-source games (only original games for now - for OSS games remakes check <a href="https://osgameclones.com/">this portal</a>).</p>
         </div>
     );
 }
