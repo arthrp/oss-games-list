cf313c77899d63d7cc3080cba2d578f344be3ecf
Arthur <arthrp@users.noreply.github.com>
2024-03-29 20:56:47 +0200

    No console log

--- minify.js
+++ minify.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 try {
     const data = fs.readFileSync('public/games.json', 'utf8');
     const minified = data.replace(/\n|\s{2,}/g, '');
-    console.log(minified);
+    // console.log(minified);
     fs.writeFileSync('public/games_min.json', minified);
    } catch (err) {
     console.error(err);
