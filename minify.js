d3fd7dd2abf12bcc46d03b9c7a9ae4d0701d95c5
Arthur <arthrp@users.noreply.github.com>
2024-03-29 20:56:21 +0200

    Small fixes in script

--- minify.js
+++ minify.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 
 try {
     const data = fs.readFileSync('public/games.json', 'utf8');
-    const minified = data.replace(/\n|\s/g, '');
-    // console.log(minified);
+    const minified = data.replace(/\n|\s{2,}/g, '');
+    console.log(minified);
     fs.writeFileSync('public/games_min.json', minified);
    } catch (err) {
     console.error(err);
