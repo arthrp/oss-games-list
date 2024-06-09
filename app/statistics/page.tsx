1b25969963261c3315452b525c1951e0ec61cb28
Arthur <arthrp@users.noreply.github.com>
2024-06-09 23:07:07 +0300

    Add another chart and change colors

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -6,11 +6,12 @@ import { parseISO } from "date-fns";
 import { IGameData } from "@/data/columns";
 import { getColors } from "@/helpers/colors";
 import 'bootstrap/dist/css/bootstrap.css'; 
+import css from './page.module.css'
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function Statistics() {
-    const data = {
+    const emptyData = {
         labels: [],
         datasets: [
             {
@@ -21,7 +22,8 @@ export default function Statistics() {
             },
         ],
     };
-    const [chartData, setChartData] = useState<any>(data);
+    const [licenseData, setLicenseData] = useState<any>(emptyData);
+    const [languageData, setLanguageData] = useState<any>(emptyData);
 
     const options = {
         responsive: true,
@@ -45,8 +47,10 @@ export default function Statistics() {
                 ...item,
                 firstReleaseDate: parseISO(item.firstReleaseDate),
               }));
-            const c = getChartData(parsedData)
-            setChartData(c);
+            const c = getLicenseChartData(parsedData);
+            const l = getLanguagesChartData(parsedData);
+            setLicenseData(c);
+            setLanguageData(l);
           };
 
         fetchData();
@@ -54,16 +58,24 @@ export default function Statistics() {
     }, []);
 
     return (
-    <div className="container"  style={{ height: '100%', maxHeight: 600 }}>
+    <div className="container">
         <h1 className="mb-5 mt-5">Statistics</h1>
-        <Pie data={chartData} options={options} />
+        
+        <div className={css.chartContainer}>
+            <h2 className={css.subheader}>Licenses</h2>
+            <Pie data={licenseData} options={options} />
+        </div>
+
+        <div className={css.chartContainer} style={{ marginTop: 70 }}>
+            <h2 className={css.subheader}>Primary language</h2>
+            <Pie data={languageData} options={options} />
+        </div>
     </div>
     )
 }
 
-function getChartData(gameData: IGameData[]){
-    
-    const top = gameData.map(e => e.codeLicense).reduce<{ [key: string]: number }>((acc,val) => { 
+function getLanguagesChartData(gameData: IGameData[]){
+    const topLangs = gameData.map(e => e.langs[0]).reduce<{ [key: string]: number }>((acc,val) => { 
         if (!acc[val]) {
             acc[val] = 0;
           }
@@ -71,14 +83,30 @@ function getChartData(gameData: IGameData[]){
           return acc;
     }, {});
 
-    const bgColors = getColors(Object.keys(top).length, 0.6);
-    const borderColors = getColors(Object.keys(top).length, 1);
-    const data = {
-        labels: Object.keys(top),
+    return getData(topLangs);
+}
+
+function getLicenseChartData(gameData: IGameData[]){
+    const topLicenses = gameData.map(e => e.codeLicense).reduce<{ [key: string]: number }>((acc,val) => { 
+        if (!acc[val]) {
+            acc[val] = 0;
+          }
+          acc[val]++;
+          return acc;
+    }, {});
+
+    return getData(topLicenses);
+}
+
+function getData(data: {[key: string]: number}){
+    const bgColors = getColors(Object.keys(data).length, 0.7);
+    const borderColors = getColors(Object.keys(data).length, 1);
+    const resultData = {
+        labels: Object.keys(data),
         datasets: [
             {
                 label: 'Count',
-                data: Object.values(top),
+                data: Object.values(data),
                 backgroundColor: bgColors,
                 borderColor: borderColors,
                 borderWidth: 1,
@@ -86,5 +114,5 @@ function getChartData(gameData: IGameData[]){
         ],
     };
 
-    return data;
+    return resultData;
 }
