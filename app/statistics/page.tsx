83ec6d18fc84057ceeb1fa3f6b7e70364e9f6c72
Arthur <arthrp@users.noreply.github.com>
2024-06-25 00:09:36 +0300

    Add other to charts when there are more than 7 items

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -10,6 +10,11 @@ import css from './page.module.css'
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
+interface CountableUnit {
+    label: string;
+    count: number;
+}
+
 export default function Statistics() {
     const emptyData = {
         labels: [],
@@ -75,38 +80,54 @@ export default function Statistics() {
 }
 
 function getLanguagesChartData(gameData: IGameData[]){
-    const topLangs = gameData.map(e => e.langs[0]).reduce<{ [key: string]: number }>((acc,val) => { 
-        if (!acc[val]) {
-            acc[val] = 0;
-          }
-          acc[val]++;
-          return acc;
-    }, {});
+    const topLangs = gameData.map(e => e.langs[0]).reduce<CountableUnit[]>((acc: CountableUnit[], val: string) => {
+        return getLabelCounts(acc, val);
+    }, []);
+
+    if(topLangs.length > 7) return getDataWithOther(topLangs);
 
     return getData(topLangs);
 }
 
 function getLicenseChartData(gameData: IGameData[]){
-    const topLicenses = gameData.map(e => e.codeLicense).reduce<{ [key: string]: number }>((acc,val) => { 
-        if (!acc[val]) {
-            acc[val] = 0;
-          }
-          acc[val]++;
-          return acc;
-    }, {});
+    const topLicenses = gameData.map(e => e.codeLicense).reduce<CountableUnit[]>((acc: CountableUnit[], val: string) => { 
+        return getLabelCounts(acc, val);
+    }, []);
+
+    if(topLicenses.length > 7) return getDataWithOther(topLicenses);
 
     return getData(topLicenses);
 }
 
-function getData(data: {[key: string]: number}){
-    const bgColors = getColors(Object.keys(data).length, 0.7);
-    const borderColors = getColors(Object.keys(data).length, 1);
+function getDataWithOther(data: CountableUnit[]){
+    const sorted = data.sort((a,b) => b.count - a.count);
+    let totalOther = 0;
+    for (let i = 7; i < sorted.length; i++){
+        totalOther += sorted[i].count;
+    }
+    const res = sorted.slice(0,7);
+    res[7] = { label: "Other", count: totalOther };
+    console.log(res[7]);
+    return getData(res);
+}
+
+function getLabelCounts(acc: CountableUnit[], val: string){
+    const existing = acc.filter(x => x.label === val);
+    if(existing.length === 0) acc.push({ label: val, count: 1 })
+    else existing[0].count++;
+    
+    return acc;    
+}
+
+function getData(data: CountableUnit[]){
+    const bgColors = getColors(data.length, 0.7);
+    const borderColors = getColors(data.length, 1);
     const resultData = {
-        labels: Object.keys(data),
+        labels: data.map(d => d.label),
         datasets: [
             {
                 label: 'Count',
-                data: Object.values(data),
+                data: data.map(d => d.count),
                 backgroundColor: bgColors,
                 borderColor: borderColors,
                 borderWidth: 1,
