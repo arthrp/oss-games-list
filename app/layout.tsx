14e563361e6b77e16aaadb138e17448f8bebc956
Arthur <arthrp@users.noreply.github.com>
2025-03-30 19:59:05 +0300

    Fix viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/header/header'
@@ -9,7 +9,11 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Open-source game list',
   description: 'Curated list of OSS games',
-  viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
 }
 
 export default function RootLayout({
